ab513da25fb553fbb4dd11d810dab5cac6f0b254
Emilio Jaluff <jaluff@gmail.com>
2021-07-13 16:43:00 -0300

    mi primer commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  exports: [
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+  ]
+})
+export class ComponentsModule { }
